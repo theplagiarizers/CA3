fd52fb3df64420edde6017235d77d87bb63d341d
John Doe <johndoe@email.com>
2023-09-19 11:18:50 +0500

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route ('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello Worrld!'
 @app.route('/bmi', methods=['POST'])
 def calculate_bmi():
     data = request.get_json()
